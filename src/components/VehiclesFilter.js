f5149bd1b048d87a65b1f3e271d7edd81c416958
Allen Catipon <allen.catipon@softvision.com>
2022-04-17 20:23:09 +0800

    update VehiclesFilter.js

--- src/components/VehiclesFilter.js
+++ src/components/VehiclesFilter.js
@@ -12,10 +12,9 @@ const VehiclesFilter = (props) => {
       <div className='vehicles-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+          <option value='Honda'>Honda</option>
+          <option value='Ducati'>Ducati</option>
+          <option value='Aprilla'>Aprilla</option>
         </select>
       </div>
     </div>
