30c76d594004e321520449c554b33f2997d1bbf8
Allen Catipon <allen.catipon@softvision.com>
2022-04-17 16:35:30 +0800

    convert class to functional component

--- src/components/VehicleComponent.js
+++ src/components/VehicleComponent.js
@@ -1,56 +1,45 @@
-import React from 'react';
-import VehicleService from '../services/VehicleService';
+import React, { useState, useEffect } from "react";
+import VehicleService from "../services/VehicleService";
+import VehiclesFilter from "./VehiclesFilter";
 
-class VehicleComponent extends React.Component {
+const VehicleComponent = (props) => {
+  const [filteredVehicle, setFilteredVehicle] = useState("Honda");
+  const [vehicle, setVehicle] = useState([]);
+  useEffect(() => {
+    VehicleService.getVehicle().then((response) => {
+      setVehicle(response.data);
+    });
+  }, []);
 
-    constructor(props){
-        super(props)
-        this.state = {
-            vehicle:[]
-        }
-    }
+  const filterChangeHandler = selectedVehicle => {
+    setFilteredVehicle(selectedVehicle);
+  };
 
-    componentDidMount(){
-        VehicleService.getVehicle().then((response) => {
-            this.setState({ vehicle: response.data})
-        });
-    }
+  return (
+    <div>
+      <h1 className="text-center"> Vehicle List</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <td> Id</td>
+            <td> Color Name</td>
+            <td> Make Name</td>
+            <td> Displacement</td>
+          </tr>
+        </thead>
+        <tbody>
+          {vehicle.map((motor) => (
+            <tr key={motor.id}>
+              <td> {motor.id}</td>
+              <td> {motor.color}</td>
+              <td> {motor.make}</td>
+              <td> {motor.displacement}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    render (){
-        return (
-            <div>
-                <h1 className = "text-center"> Vehicle List</h1>
-                <table className = "table table-striped">
-                    <thead>
-                        <tr>
-
-                            <td> Id</td>
-                            <td> Color Name</td>
-                            <td> Make Name</td>
-                            <td> Displacement</td>
-                        </tr>
-
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.vehicle.map(
-                                motor => 
-                                <tr key = {motor.id}>
-                                     <td> {motor.id}</td>   
-                                     <td> {motor.color}</td>   
-                                     <td> {motor.make}</td>   
-                                     <td> {motor.displacement}</td>   
-                                </tr>
-                            )
-                        }
-
-                    </tbody>
-                </table>
-
-            </div>
-
-        )
-    }
-}
-
-export default VehicleComponent
+export default VehicleComponent;
