fc111c393046bbf2ece3edf1cb3bb55c04f0fb65
Allen Catipon <allen.catipon@softvision.com>
2022-04-19 22:27:59 +0800

    added change handler for filter drop down, search text box, search button

--- src/components/VehiclesFilter.js
+++ src/components/VehiclesFilter.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../ui/Button';
 
 import './VehiclesFilter.css';
 
 const VehiclesFilter = (props) => {
-  const dropdownChangeHandler = (event) => {
+  const [searchValue, setSearchValue] = useState({
+    enteredText: '',
+    selectedFilter: ''
+  });
+
+  const filterByChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
+    setSearchValue((prevState) => {
+      return { ...prevState, selectedFilter: event.target.value };
+    });
+  };
+
+  const searchTextChangeHandler = (event) => {
+    setSearchValue((prevState) => {
+      return { ...prevState, enteredText: event.target.value };
+    });
+  };
+
+  const searchHandler = (event) => {
+    event.preventDefault();
+    console.log(searchValue);
   };
 
   return (
-    <div className='vehicles-filter'>
-      <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='Honda'>Honda</option>
-          <option value='Ducati'>Ducati</option>
-          <option value='Aprilla'>Aprilla</option>
-        </select>
-        <input type="text" id ="searchValue"/>
-        <Button>Search</Button>
-    </div>
+    <form onSubmit={searchHandler}>
+      <div className='vehicles-filter'>
+        <select value={props.selected} onChange={filterByChangeHandler}>
+            <option value="" selected disabled hidden>Filter By</option>
+            <option value='Honda'>Honda</option>
+            <option value='Ducati'>Ducati</option>
+            <option value='Aprilla'>Aprilla</option>
+          </select>
+          <input type="text" id ="searchValue" onChange={searchTextChangeHandler} / >
+          <Button type='submit'>Search</Button>
+      </div>
+    </form>
   );
 };
 
